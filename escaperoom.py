09b1b61abc52f39fe74b84420eeec12417cd6f82
sMailund <simen.m.s@hotmail.com>
2020-08-19 19:49:17 +0100

    init

---
+++ escaperoom.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+from gpiozero import LED
+
+codes = {
+        "2963": LED(20),
+        "8246": LED(21)
+}
+
+for pin in codes:
+    codes[pin].off()
+
+while True:
+    val = ""
+    try:
+        val = input()
+    except Exception as e:
+        print("no input")
+    pin = codes.get(val)
+    if pin is not None:
+        pin.on()
+        print("on")
+    else:
+        print("no match")
+
+    if val == "stop":
+        print("stop")
+        exit()
